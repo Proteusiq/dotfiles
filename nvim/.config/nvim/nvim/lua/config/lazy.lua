afb23819aeaabd4c26caac39b34f0b2c1311f0a0
proteusiq <praysonpi@gmail.com>
2024-06-15 10:09:05 +0200

    lazyvim config

--- nvim/.config/nvim/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/nvim/lua/config/lazy.lua
@@ -15,6 +15,10 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.test.core" },
+    { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.util.project" },
     -- import/override with your plugins
     { import = "plugins" },
   },
