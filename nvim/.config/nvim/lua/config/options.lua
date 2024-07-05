345e0e369221f69032f5f7a10c9f8df0a01b3975
proteusiq <praysonpi@gmail.com>
2024-07-05 20:48:14 +0200

    restore common stow flow. neovim is under stow instead of ln

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,6 +1,3 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-
--- main editor configs
-vim.g.python_host_prog = os.getenv("HOME") .. "/.virtualenvs/nvim/bin/python"
