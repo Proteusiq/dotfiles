3aa04716087131cd6fbfa5d2857ed4c7f8195532
proteusiq <praysonpi@gmail.com>
2024-07-05 11:04:32 +0200

    added readme.md rendering

---
+++ nvim/.config/nvim/nvim/lua/plugins/readme.lua
@@ -0,0 +1,11 @@
+return {
+  "MeanderingProgrammer/markdown.nvim",
+  name = "render-markdown",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter", -- Mandatory
+    "nvim-tree/nvim-web-devicons", -- Optional but recommended
+  },
+  config = function()
+    require("render-markdown").setup({})
+  end,
+}
