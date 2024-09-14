d16420c397a04bec371c8c9b153e5edf0587c362
proteusiq <praysonpi@gmail.com>
2024-09-14 16:14:16 +0200

    it is working for class, test, and module

--- nvim/.config/nvim/lua/plugins/debugging.lua
+++ nvim/.config/nvim/lua/plugins/debugging.lua
@@ -1,8 +1,9 @@
 
+
 return {
   "mfussenegger/nvim-dap-python",
   keys = {
-    -- Existing key mapping for debugging tests
+    -- Key mapping for debugging the current test method
     {
       mode = "n",
       "<leader>df",
@@ -11,23 +12,51 @@ return {
       end,
       desc = "Debug Python Test Method",
     },
-    -- New key mapping to debug the current method (function) under the cursor
-    {
-      mode = "n",
-      "<leader>dm",
-      function()
-        require("dap-python").debug_method()
-      end,
-      desc = "Debug Python Method",
-    },
-    -- New key mapping to debug the current class under the cursor
+    -- Key mapping for debugging the current test class
     {
       mode = "n",
       "<leader>dc",
       function()
-        require("dap-python").debug_class()
+        require("dap-python").test_class()
       end,
-      desc = "Debug Python Class",
+      desc = "Debug Python Test Class",
+    },
+    -- Key mapping for testing the current file (module)
+    {
+      mode = "n",
+      "<leader>dm",
+      function()
+        require("dap-python").test_module()
+      end,
+      desc = "Debug Python Test Module",
+    },
+    -- Key mapping for debugging the entire file (non-test code)
+    {
+      mode = "n",
+      "<leader>dr",
+      function()
+        require("dap-python").debug_file()
+      end,
+      desc = "Debug Python File",
+    },
+    -- Custom key mapping to debug the function under the cursor (non-test code)
+    {
+      mode = "n",
+      "<leader>dfn",
+      function()
+        local dap = require("dap")
+        local python = require("dap-python")
+        local utils = require("dap-python.utils")
+        local file_path = vim.fn.expand("%:p")
+        local line_num = vim.fn.line(".")
+        local func_name = utils.get_func_name_at_line(file_path, line_num)
+        if func_name then
+          python.debug_at_point()
+        else
+          print("No function found under cursor.")
+        end
+      end,
+      desc = "Debug Function Under Cursor",
     },
   },
   config = function()
