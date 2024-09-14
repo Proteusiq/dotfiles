3cfb81dfd2e284796268f9e28e13259406008b0e
proteusiq <praysonpi@gmail.com>
2024-09-14 16:00:20 +0200

    works for tests and class

--- nvim/.config/nvim/lua/plugins/debugging.lua
+++ nvim/.config/nvim/lua/plugins/debugging.lua
@@ -2,98 +2,37 @@
 return {
   "mfussenegger/nvim-dap-python",
   keys = {
+    -- Existing key mapping for debugging tests
     {
       mode = "n",
       "<leader>df",
       function()
-        require("dap").continue()
+        require("dap-python").test_method()
       end,
-      desc = "Debug current file",
+      desc = "Debug Python Test Method",
     },
+    -- New key mapping to debug the current method (function) under the cursor
     {
       mode = "n",
       "<leader>dm",
       function()
-        local dap = require("dap")
-        local home = os.getenv("HOME")
-        local python_path = home .. "/.virtualenvs/debugpy/bin/python"
-        dap.run({
-          name = "Debug Method",  -- Added name field
-          type = "python",
-          request = "launch",
-          module = "pytest",
-          args = {
-            "-v",
-            vim.fn.expand("%:p"),
-            "--capture=no",
-            "--no-header",
-            "--no-summary",
-            "-k",
-            vim.fn.expand("<cword>"),
-          },
-          pythonPath = python_path,
-          cwd = vim.fn.getcwd(),
-          env = {
-            PYTHONPATH = vim.fn.getcwd(),
-          },
-        })
+        require("dap-python").debug_method()
       end,
-      desc = "Test method under cursor",
+      desc = "Debug Python Method",
     },
+    -- New key mapping to debug the current class under the cursor
     {
       mode = "n",
       "<leader>dc",
       function()
-        require("dap-python").test_method()
-       end,
-      desc = "Debug Test",
-    },
-    {
-      mode = "v",
-      "<leader>ds",
-      function()
-        require("dap-python").debug_selection()
+        require("dap-python").debug_class()
       end,
-      desc = "Debug selected code",
+      desc = "Debug Python Class",
     },
   },
   config = function()
-    local home = os.getenv("HOME")
-    local python_path = home .. "/.virtualenvs/debugpy/bin/python"
-
-    local dap = require("dap")
-    local dap_python = require("dap-python")
-
-    -- Set DAP log level to DEBUG
-    dap.set_log_level("DEBUG")
-
-    -- Setup dap-python with your Python interpreter
-    dap_python.setup(python_path)
-    dap_python.test_runner = "pytest"
-
-    -- Configure the DAP adapter for Python
-    dap.adapters.python = {
-      type = "executable",
-      command = python_path,
-      args = { "-m", "debugpy.adapter" },
-    }
-
-    -- Define configurations for debugging Python files
-    dap.configurations.python = {
-      {
-        type = "python",
-        request = "launch",
-        name = "Launch file",
-        program = "${file}",
-        pythonPath = function()
-          return python_path
-        end,
-        cwd = "${workspaceFolder}",
-        env = {
-          PYTHONPATH = "${workspaceFolder}",
-        },
-      },
-    }
+    require("dap-python").setup(vim.g.python3_host_prog)
+    require("dap-python").test_runner = "pytest"
   end,
 }
 
