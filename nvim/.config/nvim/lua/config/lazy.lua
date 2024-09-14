2bc1e718280839a0264969be0e41189676cbcee1
proteusiq <praysonpi@gmail.com>
2024-09-14 14:53:50 +0200

    almost there with dap

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -14,7 +14,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
 -- Set up autocmd in Lua
 vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
   pattern = "*",
--- nvim/.config/nvim/lua/plugins/debugging.lua
+++ nvim/.config/nvim/lua/plugins/debugging.lua
@@ -1,3 +1,4 @@
+
 return {
   "mfussenegger/nvim-dap-python",
   keys = {
@@ -5,12 +6,94 @@ return {
       mode = "n",
       "<leader>df",
       function()
-        require("dap-python").test_method()
+        require("dap").continue()
       end,
+      desc = "Debug current file",
+    },
+    {
+      mode = "n",
+      "<leader>dm",
+      function()
+        local dap = require("dap")
+        local home = os.getenv("HOME")
+        local python_path = home .. "/.virtualenvs/debugpy/bin/python"
+        dap.run({
+          name = "Debug Method",  -- Added name field
+          type = "python",
+          request = "launch",
+          module = "pytest",
+          args = {
+            "-v",
+            vim.fn.expand("%:p"),
+            "--capture=no",
+            "--no-header",
+            "--no-summary",
+            "-k",
+            vim.fn.expand("<cword>"),
+          },
+          pythonPath = python_path,
+          cwd = vim.fn.getcwd(),
+          env = {
+            PYTHONPATH = vim.fn.getcwd(),
+          },
+        })
+      end,
+      desc = "Test method under cursor",
+    },
+    {
+      mode = "n",
+      "<leader>dc",
+      function()
+        require("dap-python").test_method()
+       end,
+      desc = "Debug Test",
+    },
+    {
+      mode = "v",
+      "<leader>ds",
+      function()
+        require("dap-python").debug_selection()
+      end,
+      desc = "Debug selected code",
     },
   },
   config = function()
-    require("dap-python").setup(vim.g.python3_host_prog)
-    require("dap-python").test_runner = "pytest"
+    local home = os.getenv("HOME")
+    local python_path = home .. "/.virtualenvs/debugpy/bin/python"
+
+    local dap = require("dap")
+    local dap_python = require("dap-python")
+
+    -- Set DAP log level to DEBUG
+    dap.set_log_level("DEBUG")
+
+    -- Setup dap-python with your Python interpreter
+    dap_python.setup(python_path)
+    dap_python.test_runner = "pytest"
+
+    -- Configure the DAP adapter for Python
+    dap.adapters.python = {
+      type = "executable",
+      command = python_path,
+      args = { "-m", "debugpy.adapter" },
+    }
+
+    -- Define configurations for debugging Python files
+    dap.configurations.python = {
+      {
+        type = "python",
+        request = "launch",
+        name = "Launch file",
+        program = "${file}",
+        pythonPath = function()
+          return python_path
+        end,
+        cwd = "${workspaceFolder}",
+        env = {
+          PYTHONPATH = "${workspaceFolder}",
+        },
+      },
+    }
   end,
 }
+
