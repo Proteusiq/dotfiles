6687bfe5e025dc80522fdc32202a655d94f20f88
proteusiq <praysonpi@gmail.com>
2025-01-01 12:00:55 +0100

    still working progres

--- bin/peak.py
+++ bin/peak.py
@@ -8,8 +8,9 @@
 # ]
 # ///
 """Peak onto Tables in DB WIP"""
-
+ 
 import random
+from enum import Enum
 from pathlib import Path
 from typing import Annotated
 
@@ -24,21 +25,42 @@ app = typer.Typer(name="Peak")
 console = Console(theme=Theme({"repr.number": "bold green blink"}))
 print = console.print
 
-STYLES = ["cyan", "magenta", "green", "blue",]
+STYLES = [
+    "cyan",
+    "magenta",
+    "green",
+    "blue",
+]
+
+show_tables = {
+    "sqlite": "SHOW tables;",
+    "postgres": """
+            SELECT table_name 
+            FROM information_schema.tables
+            WHERE table_schema = 'public'; 
+          """,
+}
+
+class Kind(str, Enum):
+    sqlite = "sqlite"
+    postgres = "postgres"
 
 
 @app.command()
 def open(
     source: Annotated[Path, typer.Argument(help="data source uri")],
     get: Annotated[str, typer.Option(help="table name")] = "",
+    limit: Annotated[int, typer.Option(help="number of show rows")] = 5, 
+    kind: Annotated[Kind, typer.Option(help="database kind")] = Kind.sqlite,
 ):
     if not get:
         get = source.stem
         with duckdb.connect(source) as con:
-            results = con.sql("SHOW tables;")
+            results = con.sql(show_tables.get(kind, "sqlite"))
             table = Table(
                 Column(header="Tables", justify="right", style="cyan"),
-                title=f"\n{source.name}",)
+                title=f"\n{source.name}",
+            )
             for row in results.fetchall():
                 table.add_row(row[0])
 
@@ -47,7 +69,7 @@ def open(
 
     table = Table(title=get)
     with duckdb.connect(source) as con:
-        results = con.sql(f"SELECT * FROM {get};")
+        results = con.sql(f"SELECT * FROM {get} LIMIT {limit};")
 
         for column in results.columns:
             table.add_column(column, style=random.choice(STYLES))
@@ -56,6 +78,7 @@ def open(
             table.add_row(*row)
 
         print(table)
+
 
 
 if __name__ == "__main__":
