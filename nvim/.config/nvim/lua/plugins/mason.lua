046ffdbd277ad3817ecb245738da8f7da365284a
Prayson W. Daniel <prdani@norlys.dk>
2025-10-20 11:06:34 +0200

    Add opencode fix #121 and # 113

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,6 +1,6 @@
 return {
-  "wmason-org/mason.nvim",
-  "wmason-org/mason-lspconfig.nvim",
+  "mason-org/mason.nvim",
+  "mason-org/mason-lspconfig.nvim",
   "neovim/nvim-lspconfig",
   opts = {
     ensure_installed = {
