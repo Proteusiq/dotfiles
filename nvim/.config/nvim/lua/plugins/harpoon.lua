56e84e45f46e2df2e9d1cf31de403673d9808b7c
Prayson W. Daniel <prdani@norlys.dk>
2025-11-13 10:51:10 +0100

    refactor: move plugin keymaps to their respective files
    Resolves #130 by moving telescope and harpoon keybindings from config/keymaps.lua to their respective plugin configuration files. This ensures better organization and makes it easier to manage or remove plugin-specific keybindings.
    
    Changes:
    - Move telescope keybindings (<leader>f*) to plugins/telescope.lua with config hook
    - Move harpoon keybindings to plugins/harpoon.lua with config hook and harpoon setup
    - Resolve <C-h> conflict: change harpoon number selects from <C-h/t/n/s> to <leader>1-4
    - Keep only general editor keybindings (window navigation) in config/keymaps.lua
    - Update harpoon to depend on telescope.nvim for integration
    - Add descriptions to all keybindings for better discoverability

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -1,5 +1,69 @@
 return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+  config = function()
+    local harpoon = require("harpoon")
+
+    -- Setup harpoon
+    harpoon:setup()
+
+    -- Harpoon keymaps
+    vim.keymap.set("n", "<leader>a", function()
+      harpoon:list():add()
+    end, { desc = "Add file to harpoon" })
+
+    vim.keymap.set("n", "<C-e>", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = "Toggle harpoon menu" })
+
+    vim.keymap.set("n", "<leader>1", function()
+      harpoon:list():select(1)
+    end, { desc = "Harpoon select 1" })
+
+    vim.keymap.set("n", "<leader>2", function()
+      harpoon:list():select(2)
+    end, { desc = "Harpoon select 2" })
+
+    vim.keymap.set("n", "<leader>3", function()
+      harpoon:list():select(3)
+    end, { desc = "Harpoon select 3" })
+
+    vim.keymap.set("n", "<leader>4", function()
+      harpoon:list():select(4)
+    end, { desc = "Harpoon select 4" })
+
+    vim.keymap.set("n", "<C-S-P>", function()
+      harpoon:list():prev()
+    end, { desc = "Harpoon previous" })
+
+    vim.keymap.set("n", "<C-S-N>", function()
+      harpoon:list():next()
+    end, { desc = "Harpoon next" })
+
+    -- Telescope + Harpoon integration
+    local conf = require("telescope.config").values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+      require("telescope.pickers")
+        .new({}, {
+          prompt_title = "Harpoon",
+          finder = require("telescope.finders").new_table({
+            results = file_paths,
+          }),
+          previewer = conf.file_previewer({}),
+          sorter = conf.generic_sorter({}),
+        })
+        :find()
+    end
+
+    -- Note: This overwrites the harpoon quick menu toggle (C-e)
+    -- Uncomment the line below to use telescope picker instead of harpoon quick menu
+    -- vim.keymap.set("n", "<C-e>", function()
+    --   toggle_telescope(harpoon:list())
+    -- end, { desc = "Open harpoon window (telescope)" })
+  end,
 }
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,13 @@
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local builtin = require("telescope.builtin")
+
+    -- Telescope keymaps
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+  end,
 }
