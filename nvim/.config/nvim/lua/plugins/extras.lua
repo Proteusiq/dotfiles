ba3341bcc6654101cdadaf04cb97070310578f47
Prayson W. Daniel <praysonpi@gmail.com>
2025-12-11 12:29:26 +0100

    neovim restored

---
+++ nvim/.config/nvim/lua/plugins/extras.lua
@@ -0,0 +1,16 @@
+-- LazyVim Extras
+-- Import extras here instead of using :LazyExtras UI
+return {
+  { import = "lazyvim.plugins.extras.coding.yanky" },
+  { import = "lazyvim.plugins.extras.dap.core" },
+  { import = "lazyvim.plugins.extras.editor.leap" },
+  { import = "lazyvim.plugins.extras.editor.neo-tree" },
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.rust" },
+  { import = "lazyvim.plugins.extras.test.core" },
+  { import = "lazyvim.plugins.extras.ui.mini-animate" },
+  { import = "lazyvim.plugins.extras.util.dot" },
+  { import = "lazyvim.plugins.extras.util.gh" },
+  { import = "lazyvim.plugins.extras.util.gitui" },
+  { import = "lazyvim.plugins.extras.util.octo" },
+}
--- nvim/.config/nvim/lua/plugins/telescopedap.lua
+++
@@ -1,3 +0,0 @@
-return {
-  "nvim-telescope/telescope-dap.nvim",
-}
