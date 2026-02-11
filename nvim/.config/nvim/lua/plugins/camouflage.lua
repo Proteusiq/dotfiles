05c13e651f849fafd3e4f904b4395100c2010ecc
Prayson W. Daniel <praysonpi@gmail.com>
2026-02-11 07:20:53 +0100

    fix: only mask sensitive keys in camouflage.nvim

--- nvim/.config/nvim/lua/plugins/camouflage.lua
+++ nvim/.config/nvim/lua/plugins/camouflage.lua
@@ -4,5 +4,36 @@ return {
   opts = {},
   keys = {
     { '<leader>ct', '<cmd>CamouflageToggle<cr>', desc = 'Toggle Camouflage' },
+    { '<leader>cr', '<cmd>CamouflageReveal<cr>', desc = 'Reveal Line' },
+    { '<leader>cy', '<cmd>CamouflageYank<cr>', desc = 'Yank Value' },
   },
+  config = function(_, opts)
+    local camouflage = require('camouflage')
+    camouflage.setup(opts)
+
+    local sensitive_patterns = {
+      'api[_-]?key',
+      'apikey',
+      'secret',
+      'password',
+      'passwd',
+      'token',
+      'credential',
+      'auth',
+      'private[_-]?key',
+      'access[_-]?key',
+      'client[_-]?secret',
+      'bearer',
+    }
+
+    camouflage.on('variable_detected', function(_, var)
+      local key_lower = var.key:lower()
+      for _, pattern in ipairs(sensitive_patterns) do
+        if key_lower:match(pattern) then
+          return true
+        end
+      end
+      return false
+    end)
+  end,
 }
